75e74ee1c69fcc4fc7e5f6c25590c49966598ddf
shalidesh <shalidesh970@gmail.com>
2023-09-29 16:34:22 +0530

    dashboard complete

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -26,23 +26,32 @@ import { auth, database } from '../config/firebase';
 export default function Dashboard() {
 
     // Array of colors for the pie chart
-    const colors = ["rgba(131, 167, 234, 1)", "#F00", "#red", "#ffffff", "rgb(0, 0, 255)", "#0FF"];
+    const colors = ["rgba(131, 167, 234, 1)", "#F00", "yellow", "#ffffff", "rgb(0, 0, 255)", "#0FF"];
 
     const [data1, setData1] = useState([]);
+    const [bacterial_spot, Setbacterial_spot] = useState(0);
+    const [early_blight, Setearly_blight] = useState(0);
+    const [late_blight, Setlate_blight] = useState(0);
+    const [leaf_mold, Setleaf_mold] = useState(0);
+    const [mosaic_virus, Setmosaic_virus] = useState(0);
+    const [septoria_leaf_spot, Setseptoria_leaf_spot] = useState(0);
+    const [spider_mites, Setspider_mites] = useState(0);
+    const [target_spot, Settarget_spot] = useState(0);
+    const [yellow_leaf_curl, Setyellow_leaf_curl] = useState(0);
 
     const screenWidth = Dimensions.get("window").width;
 
-    const data = {
-        labels: ["January", "February", "March", "April"],
+    const [data, setData] = useState({
+        labels: ["Jan", "Feb", "Mar", "Apr","Jan", "Feb", "Mar", "Apr"],
         datasets: [
           {
-            data: [20, 45, 28, 80],
+            data: [20, 45, 28, 80,20, 45, 28, 80],
             color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
             strokeWidth: 3 // optional
           }
         ],
         legend: ["Rainy Days"] // optional
-      };
+      });
 
       const data3 = {
         labels: ["January", "February", "March", "April", "May", "June"],
@@ -73,7 +82,7 @@ export default function Dashboard() {
         backgroundGradientTo: "#08130D",
         backgroundGradientToOpacity: 0.5,
         color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-        strokeWidth: 2, // optional, default 3
+        strokeWidth: 3, // optional, default 3
         barPercentage: 0.5,
         useShadowColorFromDataset: false // optional
       };
@@ -86,6 +95,12 @@ export default function Dashboard() {
             fetchDetections(user.uid);
           }
       }, []);
+
+      // Helper function to get month name from month number
+      function getMonthName(monthNumber) {
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        return monthNames[monthNumber - 1];
+      }
     
       function fetchDetections(userId) {
         console.log("user exist", {userId});
@@ -95,11 +110,26 @@ export default function Dashboard() {
         const q = query(collectionRef, where('userId', '==', userId));
         
         let diseaseCounts = {};
+        let recordCountsByMonth = {};
     
         const unsubscribe = onSnapshot(q, querySnapshot => {
             querySnapshot.docs.map(doc => {
                 let record = doc.data();
                 console.log("Record: ", record);
+
+                // Convert timestamp to Date object
+                let timestampInSeconds = record.timestamp.seconds;
+                let date = new Date(timestampInSeconds * 1000);
+
+                // Get the month (0-based index, so we add 1 to get a human-readable month number)
+                let month = date.getMonth() + 1;
+
+                // Increment the count for this month
+                if (month in recordCountsByMonth) {
+                    recordCountsByMonth[month]++;
+                } else {
+                    recordCountsByMonth[month] = 1;
+                }
     
                 // Increment the count for this diseaseClass
                 if (record.diseaseClass in diseaseCounts) {
@@ -111,6 +141,40 @@ export default function Dashboard() {
     
             // Log the counts
             console.log("Disease Counts: ", diseaseCounts);
+            // Log the counts
+            console.log("Record Counts By Month: ", recordCountsByMonth);
+
+             // Create labels and data arrays
+            let labels = [];
+            let data = [];
+            for (let i = 1; i <= 12; i++) {
+                labels.push(getMonthName(i));
+                data.push(recordCountsByMonth[i] || 0);
+            }
+
+            // Update state
+            setData({
+              labels: labels,
+              datasets: [
+                {
+                  data: data,
+                  color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`,
+                  strokeWidth: 3
+                }
+              ],
+              legend: ["Total Unhealthy Plants"]
+            });
+
+           
+            Setlate_blight(diseaseCounts["Late Blight"] || 0)
+            Setbacterial_spot(diseaseCounts["Bacterial Spot"] || 0)
+            Setearly_blight(diseaseCounts["Early Blight"] || 0)
+            Setleaf_mold(diseaseCounts["Leaf Mold"] || 0)
+            Setmosaic_virus(diseaseCounts["Mosaic Virus"] || 0)
+            Setseptoria_leaf_spot(diseaseCounts["Septoria Leaf Spot"] || 0)
+            Setspider_mites(diseaseCounts["Spider Mites"] || 0)
+            Settarget_spot(diseaseCounts["Target Spot"] || 0)
+            Setyellow_leaf_curl(diseaseCounts["Yellow Leaf Curl"] || 0)
 
             // Update data1 based on diseaseCounts
             const newData1 = Object.keys(diseaseCounts).map((diseaseClass, index) => ({
@@ -133,30 +197,80 @@ export default function Dashboard() {
     
     return(
 
-      <ScrollView>
-      <View style={styles.container}>
-          {/* <Text>Bezier Line Chart</Text> */}
-          <View style={styles.card}>
-              <PieChart
-                  data={data1}
-                  width={screenWidth}
-                  height={200}
-                  chartConfig={chartConfig}
-                  accessor={"population"}
-                  backgroundColor={"transparent"}
-                  paddingLeft={"0"}
-                  absolute={false}
-              />
-          </View>
-          <View style={styles.card}>
-          <LineChart
-                data={data}
-                width={screenWidth-40}
-                height={200}
-                chartConfig={chartConfig}
-                />
-          </View>
+<ScrollView>
+    <View style={styles.container}>
+       <View style={styles.titleCard}>
+        <Text style={styles.titleText}>Each Disease Plants Counts</Text>
       </View>
+      <View style={styles.rowContainer}>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Bacterial Spot</Text>
+          <Text style={styles.centerText1}>{bacterial_spot}</Text>
+        </View>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Early Blight</Text>
+          <Text style={styles.centerText1}>{early_blight}</Text>
+        </View>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Late Blight</Text>
+          <Text style={styles.centerText1}>{late_blight}</Text>
+        </View>
+      </View>
+      <View style={styles.rowContainer}>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Leaf Mold</Text>
+          <Text style={styles.centerText1}>{leaf_mold}</Text>
+        </View>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Mosaic Virus</Text>
+          <Text style={styles.centerText1}>{mosaic_virus}</Text>
+        </View>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Septoria Leaf </Text>
+          <Text style={styles.centerText1}>{septoria_leaf_spot}</Text>
+        </View>
+      </View>
+      <View style={styles.rowContainer}>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Spider Mites</Text>
+          <Text style={styles.centerText1}>{spider_mites}</Text>
+        </View>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Yellow Leaf Curl</Text>
+          <Text style={styles.centerText1}>{yellow_leaf_curl}</Text>
+        </View>
+        <View style={styles.card1}>
+          <Text style={styles.centerText}>Target Spot</Text>
+          <Text style={styles.centerText1}>{target_spot}</Text>
+        </View>
+      </View>
+      <View style={styles.titleCard}>
+        <Text style={styles.titleText}>Pie Chart</Text>
+      </View>
+      <View style={styles.card}>
+        <PieChart
+          data={data1}
+          width={screenWidth}
+          height={200}
+          chartConfig={chartConfig}
+          accessor={"population"}
+          backgroundColor={"transparent"}
+          paddingLeft={"0"}
+          absolute={false}
+        />
+      </View>
+      <View style={styles.titleCard}>
+        <Text style={styles.titleText}>Line Chart</Text>
+      </View>
+      <View style={styles.card}>
+        <LineChart
+          data={data}
+          width={screenWidth-30}
+          height={200}
+          chartConfig={chartConfig}
+        />
+      </View>
+    </View>
   </ScrollView>
 
         
@@ -169,11 +283,47 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
   },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
   card: {
-    backgroundColor: '#18d10a',
+    flex: 1,
+    backgroundColor: '#638ae6',
     borderRadius: 15,
+    padding: 1,
+    elevation: 1,
+    marginTop:5,
+    marginHorizontal:5
+  },
+  card1: {
+    flex: 1,
+    backgroundColor: '#638ae6',
+    borderRadius: 0,
     padding: 10,
     elevation: 1,
-    marginTop:5
+    marginTop:5,
+    marginHorizontal:5
+  },
+  centerText: {
+    textAlign: 'center',
+  },
+  centerText1: {
+    textAlign: 'center',
+    fontSize:30,
+    fontWeight:'bold'
+  },
+  titleCard: {
+    backgroundColor: '#18d10a',
+    borderRadius: 0,
+    padding: 10,
+    elevation: 1,
+    marginTop:5,
+    marginHorizontal:5
+  },
+  titleText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
